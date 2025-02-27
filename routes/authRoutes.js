cea498e611b6afe6ce44de9e2fbfb12ebe61da40
sarvasva86 <suchipatel5486@gmail.com>
2025-02-27 13:32:16 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// ✅ Signup Route
 router.post("/register", authController.register);
-router.post("/login", authController.login);
 
+router.post("/login", authController.login);
 module.exports = router;
