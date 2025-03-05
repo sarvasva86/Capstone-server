93a9341bec1262cf32ed9b877a6a55e9e5de3204
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 12:37:16 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
-const mongoose = require("mongoose");  // Import Mongoose
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },  // Name is required
-  email: { type: String, required: true, unique: true },  // Email must be unique
-  password: { type: String, required: true },  // Store hashed password
-}, { timestamps: true });  // Automatically add createdAt and updatedAt fields
+  name: { 
+    type: String, 
+    required: [true, "Name is required"] 
+  },
+  email: { 
+    type: String, 
+    required: [true, "Email is required"],
+    unique: [true, "Email already exists"] 
+  },
+  password: { 
+    type: String, 
+    required: [true, "Password is required"] 
+  }
+}, { 
+  timestamps: true 
+});
 
-module.exports = mongoose.model("User", UserSchema);  // Export model
-
+export default mongoose.model("User", UserSchema);
