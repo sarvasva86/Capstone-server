2da5c9e28d48e3415aee39285760b9890e64901f
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 08:45:00 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,10 @@ const authController = require("../controllers/authController");
 
 // ✅ Signup Route
 router.post("/register", authController.register);
+router.post("/signup", (req, res) => { ... });
+
+module.exports = router; // Critical!
+
 
 router.post("/login", authController.login);
 module.exports = router;
