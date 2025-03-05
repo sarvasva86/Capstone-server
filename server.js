0a8a4a1237407151f1b5121cd4b9393fc8872d45
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 12:32:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import itineraryRoutes from "./routes/itineraryRoutes.js"; // Added .js extension
+import itineraryRoutes from "./routes/itineraryRoutes.js"; 
 
 // Load environment variables
 dotenv.config();
