8aed6c69c432f7ce9105790ac59261397ab2b5cf
sarvasva86 <suchipatel5486@gmail.com>
2025-03-10 11:49:49 -0400

    Update Itinerary.js

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const itinerarySchema = new mongoose.Schema({
   userId: String,
   destinations: [String],
-  activities: [String],
+  activities: [String],    // User-defined activities
   startDate: Date,
   endDate: Date,
+  savedActivities: [{ type: String }] // Tracks user preferences
 });
 
 export default mongoose.model("Itinerary", itinerarySchema);
